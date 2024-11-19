89d9715f618e9a62321211d2cda63184c856f492
lsao2000 <lahcenenligne@gmail.com>
2024-11-19 23:05:14 +0100

    search users ui & get users from database

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class IghoumaneUser {
   late final String _userId;
   late final String _firstName;
@@ -29,7 +31,26 @@ class IghoumaneUser {
       "description": _description
     };
   }
+  IghoumaneUser.fromMap(Map<String, dynamic> data){
+    _firstName = data["firstName"];
+    _lastName = data["lastName"];
+    _userId = data["user_id"];
+    Timestamp date = data["createdAt"];
+    _createdAt = date.toDate();
+    _password = "";
+    _phoneNumber = "";
+  }
 
+  IghoumaneUser.getUserFromQuerySnapshot(
+      QueryDocumentSnapshot<Map<String, dynamic>> user) {
+    _firstName = user["firstName"];
+    _lastName = user["lastName"];
+    _userId = user["user_id"];
+    Timestamp date = user["createdAt"];
+    _createdAt = date.toDate();
+    _password = "";
+    _phoneNumber = "";
+  }
   set setUserId(String userId) {
     _userId = userId;
   }
