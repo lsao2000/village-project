06af25347da0212cdd39515c37ed3bcf7fa1cf1e
lsao2000 <lahcenenligne@gmail.com>
2024-11-21 14:55:44 +0100

    fix test failed

--- lib/controller/services/firebase_services/searchServices/search_user_services.dart
+++ lib/controller/services/firebase_services/searchServices/search_user_services.dart
@@ -3,30 +3,25 @@ import 'package:village_project/controller/services/firebase_services/user_servi
 import 'package:village_project/model/user.dart';
 
 class SearchUserServices {
-  static Future<QuerySnapshot> searchUsers(
-      {required String query}) async {
-    try {
-      final querySnapshot = await db
-          .collection("users")
-          .where("firstName", isGreaterThanOrEqualTo: query)
-          .where("firstName", isLessThanOrEqualTo: "$query,")
-          //.where("lastName", isGreaterThanOrEqualTo: query)
-          //.where("lastName", isLessThanOrEqualTo: "$query,")
-          .get();
-      return querySnapshot;
-      //List<IghoumaneUser> lstUsers = querySnapshot.docs
-      //    .map((el) => IghoumaneUser.getUserFromQuerySnapshot(el))
-      //    .toList()
-      //    .where((el) {
-      //  //return  query.contains(el.getFirstName)  || query.contains(el.getLastName);
-      //  //el.getFirstName.contains(query);
-      //  return el.getFirstName.contains(query) ||
-      //      el.getLastName.contains(query);
-      //}).toList();
-      //return lstUsers;
-    } catch (e) {
-      //print("error ${e.toString()}");
-      return [];
-    }
+  static Future<QuerySnapshot> searchUsers({required String query}) async {
+    final querySnapshot = await db
+        .collection("users")
+        .where("firstName", isGreaterThanOrEqualTo: query)
+        .where("firstName", isLessThanOrEqualTo: "$query,")
+        //.where("lastName", isGreaterThanOrEqualTo: query)
+        //.where("lastName", isLessThanOrEqualTo: "$query,")
+        .get();
+    return querySnapshot;
+    //List<IghoumaneUser> lstUsers = querySnapshot.docs
+    //    .map((el) => IghoumaneUser.getUserFromQuerySnapshot(el))
+    //    .toList()
+    //    .where((el) {
+    //  //return  query.contains(el.getFirstName)  || query.contains(el.getLastName);
+    //  //el.getFirstName.contains(query);
+    //  return el.getFirstName.contains(query) ||
+    //      el.getLastName.contains(query);
+    //}).toList();
+    //return lstUsers;
+    //print("error ${e.toString()}");
   }
 }
