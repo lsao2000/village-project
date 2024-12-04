ffee5445147a3fd180fc6aa12dd268c748ba252e
lsao2000 <lahcenenligne@gmail.com>
2024-12-04 21:17:46 +0100

    push notification & get message notification

--- functions/index.js
+++ functions/index.js
@@ -1,112 +1,128 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+// const functions = require('firebase-functions');
+// const admin = require('firebase-admin');
+import * as functions from "firebase-functions";
+import admin from "firebase-admin";
+admin.initializeApp({
+    credential: admin.credential.applicationDefault(),
+});
+//export const sendNotification = functions.firestore.onDocumentCreated(
+//    'chats/{chatId}/messages/{messageId}',
+//    (event) => {
+//        console.log(event.data.data());
+//    }
+//);
+export const sendNotification = functions.firestore
+    .onDocumentCreated("chats/{chatId}/messages/{messageId}", (changes) => {
+        console.log("----------------start function--------------------");
 
-//const {onRequest} = require("firebase-functions/v2/https");
-//const logger = require("firebase-functions/logger");
+        const doc = changes.data.data();
+        console.log(doc);
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+        const senderIdentification = doc.senderId;
+        const contentMessage = doc.msgText;
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-admin.initializeApp();
+        // Extract chatId from the context params
+        const chatId = changes.params.chatId;
 
-exports.sendNotification = functions.firestore
-  .document('chat/{chatId}/messages/{messageId}')
-  .onCreate((snap, context) => {
-    console.log('----------------start function--------------------');
+        // Retrieve the chat document to get userIds
+        admin
+            .firestore()
+            .collection("chats")
+            .doc(chatId)
+            .get()
+            .then((chatDoc) => {
+                if (!chatDoc.exists) {
+                    console.log("Chat document does not exist.");
+                    return null;
+                }
 
-    const doc = snap.data();
-    console.log(doc);
+                const userIds = [
+                    chatDoc.data().user1_id,
+                    chatDoc.data().user2_id,
+                ];
+                const receiverId = userIds.find((id) =>
+                    id !== senderIdentification
+                ); // Determine receiverId
 
-    const senderIdentification = doc.senderId;
-    const contentMessage = doc.msgText;
+                if (!receiverId) {
+                    console.log("Receiver ID not found.");
+                    return null;
+                }
 
-    // Extract chatId from the context params
-    const chatId = context.params.chatId;
+                // Get push token for the receiver
+                return admin
+                    .firestore()
+                    .collection("users")
+                    .where("user_id", "==", receiverId)
+                    .get()
+                    .then((querySnapshot) => {
+                        querySnapshot.forEach((userTo) => {
+                            console.log(
+                                `Found user to: ${userTo.data().firstName}`,
+                            );
+                            if (
+                                userTo.data().pushToken &&
+                                userTo.data().chatingWith !==
+                                    senderIdentification
+                            ) {
+                                // Get info about the sender
+                                admin
+                                    .firestore()
+                                    .collection("users")
+                                    .where(
+                                        "user_id",
+                                        "==",
+                                        senderIdentification,
+                                    )
+                                    .get()
+                                    .then((querySnapshot2) => {
+                                        querySnapshot2.forEach((userFrom) => {
+                                            console.log(
+                                                `Found user from: ${userFrom.data().firstName}`,
+                                            );
+                                            const payload = {
+                                                notification: {
+                                                    title:
+                                                        `Message from "${userFrom.data().firstName}"`,
+                                                    body: contentMessage,
+                                                    //badge: "1",
+                                                    //sound: "default",
+                                                },
+                                                token: userTo.data().pushToken,
+                                            };
 
-    // Retrieve the chat document to get userIds
-    admin
-      .firestore()
-      .collection('chat')
-      .doc(chatId)
-      .get()
-      .then(chatDoc => {
-        if (!chatDoc.exists) {
-          console.log('Chat document does not exist.');
-          return null;
-        }
-
-        const userIds = [chatDoc.data().user1_id, chatDoc.data().user2_id];
-        const receiverId = userIds.find(id => id !== senderIdentification); // Determine receiverId
-
-        if (!receiverId) {
-          console.log('Receiver ID not found.');
-          return null;
-        }
-
-        // Get push token for the receiver
-        return admin
-          .firestore()
-          .collection('users')
-          .where('user_id', '==', receiverId)
-          .get()
-          .then(querySnapshot => {
-            querySnapshot.forEach(userTo => {
-              console.log(`Found user to: ${userTo.data().firstName}`);
-              if (userTo.data().pushToken && userTo.data().chatingWith !== senderIdentification) {
-                // Get info about the sender
-                admin
-                  .firestore()
-                  .collection('users')
-                  .where('user_id', '==', senderIdentification)
-                  .get()
-                  .then(querySnapshot2 => {
-                    querySnapshot2.forEach(userFrom => {
-                      console.log(`Found user from: ${userFrom.data().firstName}`);
-                      const payload = {
-                        notification: {
-                          title: `Message from "${userFrom.data().firstName}"`,
-                          body: contentMessage,
-                          badge: '1',
-                          sound: 'default',
-                        },
-                        // token:userTo.data().pushToken
-                      };
-
-                      // Send notification to the receiver
-                      admin
-                        .messaging()
-                        // .send(payload)
-                        .sendToDevice(userTo.data().pushToken, payload)
-                        .then(response => {
-                          console.log('Successfully sent message:', response);
-                        })
-                        .catch(error => {
-                          console.log('Error sending message:', error);
+                                            // Send notification to the receiver
+                                            admin
+                                                .messaging()
+                                                .send(payload)
+                                                //.sendToDevice(userTo.data().pushToken, payload)
+                                                .then((response) => {
+                                                    console.log(
+                                                        "Successfully sent message:",
+                                                        response,
+                                                    );
+                                                })
+                                                .catch((error) => {
+                                                    console.log(
+                                                        "Error sending message:",
+                                                        error,
+                                                    );
+                                                });
+                                        });
+                                    });
+                            } else {
+                                console.log(
+                                    "Cannot find pushToken for the target user or the user is chatting with the sender.",
+                                );
+                            }
                         });
                     });
-                  });
-              } else {
-                console.log('Cannot find pushToken for the target user or the user is chatting with the sender.');
-              }
+            })
+            .catch((error) => {
+                console.log("Error retrieving chat document:", error);
             });
-          });
-      })
-      .catch(error => {
-        console.log('Error retrieving chat document:', error);
-      });
 
-    return null;
-  });
+        return null;
+    });
 
+//  // .document('chat/{chatId}/messages/{messageId}')
